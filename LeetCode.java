2712a6e20ee623faa0ea47aa0106168b84a1b18a
JackKing <iamhao888@gmail.com>
2023-06-24 00:19:48 +0800

    优化版本

--- LeetCode.java
+++ LeetCode.java
@@ -177,14 +177,12 @@ public class LeetCode {
         Arrays.sort(pairs, (a, b) -> a[0] - b[0]);
         int[] dp = new int[pairs.length];
         Arrays.fill(dp, 1);
+        int ret = dp[0];
         for (int i = 0; i < pairs.length; i++) {
             for (int j = 0; j < i; j++) {
                 if (pairs[j][1] < pairs[i][0]) dp[i] = Math.max(dp[i], dp[j] + 1);
             }
-        }
-        int ret = dp[0];
-        for (int k = 0; k < dp.length; k++) {
-            ret = Math.max(ret, dp[k]);
+            ret = Math.max(ret, dp[i]);
         }
         return ret;
     }
