4e7c67e8e8cf12a6621343d7dee52182256f7638
JackKing <iamhao888@gmail.com>
2023-06-26 15:18:45 +0800

    583. 两个字符串的删除操作

--- LeetCode.java
+++ LeetCode.java
@@ -235,6 +235,23 @@ public class LeetCode {
         return profit;
     }
 
+    //583. 两个字符串的删除操作
+    public int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+        int[][] dp = new int[n + 1][m + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return m + n - dp[n][m] * 2;
+    }
+
     public static void main(String[] args) {
         LeetCode exculpate = new LeetCode();
         String text1 = "abcde", text2 = "ace";
