9b81626cfbfbc5af9d7ad9e0795f40bd8c822e95
JackKing <iamhao888@gmail.com>
2023-06-27 17:36:28 +0800

    101. 对称二叉树

--- LeetCode.java
+++ LeetCode.java
@@ -320,6 +320,20 @@ public class LeetCode {
         return isSubtreeStartRoot(root.left, subRoot.left) && isSubtreeStartRoot(root.right, subRoot.right);
     }
 
+    //101. 对称二叉树
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        return isSymmetric(root.left, root.right);
+
+    }
+
+    private boolean isSymmetric(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) return true;
+        if (root1 == null || root2 == null) return false;
+        if (root1.val != root2.val) return false;
+        return isSymmetric(root1.left, root2.right) && isSymmetric(root1.right, root2.left);
+    }
+
     public static void main(String[] args) {
         LeetCode exculpate = new LeetCode();
         String text1 = "intention", text2 = "execution";
